a10bb7c2d7e653e37a78bdcdb18458f633a0e31c
Tyson Lindhardt <tlindhardt@arcturussoftwaredesign.com>
2026-01-30 10:53:49 -0600

    Working color sensors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hivemind/subsystems/ColorSensors.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hivemind/subsystems/ColorSensors.kt
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.hivemind.subsystems
 import com.qualcomm.robotcore.hardware.NormalizedColorSensor
 import dev.nextftc.core.subsystems.SubsystemGroup
 import dev.nextftc.ftc.ActiveOpMode.hardwareMap
+import dev.nextftc.ftc.ActiveOpMode.telemetry
 
 object ColorSensors : SubsystemGroup(
     Intake,
@@ -27,42 +28,42 @@ object ColorSensors : SubsystemGroup(
     override fun periodic() {
         var found = false
         val leftColors = leftSensor.normalizedColors
-        if (leftColors.alpha > .03) {
+        if (leftColors.alpha > .1) {
             found = true
             Fries.closeLeft.schedule()
-            if (leftColors.green > leftColors.blue) {
-                Lights.leftGreen.schedule()
-            } else {
-                Lights.leftPurple.schedule()
-            }
+//            if (leftColors.green > leftColors.blue) {
+//                Lights.leftGreen.schedule()
+//            } else {
+//                Lights.leftPurple.schedule()
+//            }
         } else {
-            Lights.leftOff.schedule()
+//            Lights.leftOff.schedule()
         }
 
         val centerColors = centerSensor.normalizedColors
-        if (centerColors.alpha > .03) {
+        if (centerColors.alpha > .1) {
             found = true
             Fries.closeCenter.schedule()
-            if (centerColors.green > centerColors.blue) {
-                Lights.centerGreen.schedule()
-            } else {
-                Lights.centerPurple.schedule()
-            }
+//            if (centerColors.green > centerColors.blue) {
+//                Lights.centerGreen.schedule()
+//            } else {
+//                Lights.centerPurple.schedule()
+//            }
         } else {
-            Lights.centerOff.schedule()
+//            Lights.centerOff.schedule()
         }
 
         val rightColors = rightSensor.normalizedColors
-        if (rightColors.alpha > .03) {
+        if (rightColors.alpha > .1) {
             found = true
             Fries.closeRight.schedule()
-            if (rightColors.green > rightColors.blue) {
-                Lights.rightGreen.schedule()
-            } else {
-                Lights.rightPurple.schedule()
-            }
+//            if (rightColors.green > rightColors.blue) {
+//                Lights.rightGreen.schedule()
+//            } else {
+//                Lights.rightPurple.schedule()
+//            }
         } else {
-            Lights.rightOff.schedule()
+//            Lights.rightOff.schedule()
         }
 
         if (found) {
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hivemind/subsystems/Lights.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hivemind/subsystems/Lights.kt
@@ -12,26 +12,26 @@ object Lights : Subsystem {
         OFF(0.0)
     }
 
-    private val leftLight = ServoEx("left_light")
+//    private val leftLight = ServoEx("left_light")
     private val centerLight = ServoEx("center_light")
-    private val rightLight = ServoEx("right_light")
+//    private val rightLight = ServoEx("right_light")
 
-    val leftOff = SetPosition(leftLight, Color.OFF.value).requires(this)
-    val leftGreen = SetPosition(leftLight, Color.GREEN.value).requires(this)
-    val leftPurple = SetPosition(leftLight, Color.PURPLE.value).requires(this)
+//    val leftOff = SetPosition(leftLight, Color.OFF.value).requires(this)
+//    val leftGreen = SetPosition(leftLight, Color.GREEN.value).requires(this)
+//    val leftPurple = SetPosition(leftLight, Color.PURPLE.value).requires(this)
 
     val centerOff = SetPosition(centerLight, Color.OFF.value).requires(this)
     val centerGreen = SetPosition(centerLight, Color.GREEN.value).requires(this)
     val centerPurple = SetPosition(centerLight, Color.PURPLE.value).requires(this)
 
-    val rightOff = SetPosition(rightLight, Color.OFF.value).requires(this)
-    val rightGreen = SetPosition(rightLight, Color.GREEN.value).requires(this)
-    val rightPurple = SetPosition(rightLight, Color.PURPLE.value).requires(this)
+//    val rightOff = SetPosition(rightLight, Color.OFF.value).requires(this)
+//    val rightGreen = SetPosition(rightLight, Color.GREEN.value).requires(this)
+//    val rightPurple = SetPosition(rightLight, Color.PURPLE.value).requires(this)
 
     override fun initialize() {
-        leftLight.position = Color.OFF.value
+//        leftLight.position = Color.OFF.value
         centerLight.position = Color.OFF.value
-        rightLight.position = Color.OFF.value
+//        rightLight.position = Color.OFF.value
     }
 
 }
